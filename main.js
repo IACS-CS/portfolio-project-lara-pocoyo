71ce7b048bc1c70f7c32b63d34238c1cab00cdff
lara-pocoyo <lara.deoliveiralascolla@innovationcharter.org>
2025-01-13 03:17:15 +0000

    jj

--- main.js
+++ main.js
@@ -1,53 +1,20 @@
-// Write JavaScript here.
-console.log("main.js loaded successfully!");
 
-/* Some sample JavaScript: this just listens 
-for any "click" on the hero element and then
-toggles the "active" class which we use to
-shift colors when they click */
-let hero = document.querySelector(".hero");
-hero.addEventListener("click", () => {
-  console.log("They clicked the hero!");
-  hero.classList.toggle("active");
+
+onload = () => {
+    const c = setTimeout(() => {
+      document.body.classList.remove("not-loaded");
+      clearTimeout(c);
+    }, 1000);
+  };
+
+  document.addEventListener("DOMContentLoaded", function() {
+    const words = document.querySelectorAll('.word');
+    let delay = 0;
+    
+    words.forEach((word, index) => {
+        setTimeout(() => {
+            word.classList.add('show');
+        }, delay);
+        delay += 500;  // Delay de 500ms para cada palavra
+    });
 });
-
-
-var options = {
-  strings: [
-    '<i>First</i> sentence.', 
-    'Hey',
-    'Welcome to my Portifolio',
-    'by',
-    'Lara Lascolla 😝'],
-  typeSpeed: 150
-};
-
-var typed = new Typed('h1', options);
-
-let currentSlide = 0;
-
-function showSlide(index) {
-  const slides = document.querySelectorAll('.carousel-item');
-  const totalSlides = slides.length;
-
-  if (index >= totalSlides) {
-    currentSlide = 0;
-  } else if (index < 0) {
-    currentSlide = totalSlides - 1;
-  } else {
-    currentSlide = index;
-  }
-
-  const carousel = document.querySelector('.carousel');
-  carousel.style.transform = `translateX(-${currentSlide * 100}%)`;
-}
-
-function moveSlide(direction) {
-  showSlide(currentSlide + direction);
-}
-
-// Show the first slide initially
-showSlide(currentSlide);
-
-
-
